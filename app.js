d935420d975368af63c123d1ae27d9f338081386
fufako <patryko.mikiciuk@gmail.com>
2022-09-23 13:50:28 +0200

    Change port

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(function (req, res, next) {
 //   res.render("error")
 // })
 
-const PORT = process.env.PORT || 5005
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
